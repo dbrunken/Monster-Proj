a81e8f53474ea30f6ff28e51b14b8278ec04be8e
dbrunken <david.brunken@startmail.com>
2022-01-12 17:27:47 -0800

    rolodex final form

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,32 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { SearchBox } from './components/search-box/search-box.comp';
+import { Cardlist } from './components/card-list/card-list-comp';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
+      monsters: [],
+      searchField: '',
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+
   render() {
+
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map()
-        }
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => {
+            this.setState({searchField: e.target.value})}}
+        />
+        <Cardlist monsters={filteredMonsters}/>
       </div>
     );
   }
